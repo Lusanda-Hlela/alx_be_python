5c6720f01a4d75b7b1a479a34c92f2785dab4e11
Lusanda <hlelalusa@gmail.com>
2024-06-22 22:01:40 +0200

    Script that calculate the annual simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,18 @@
+# Simple Interest Calculator
+
+# I: interest earned
+# P: principal amount (initial investment)
+# R: interest rate (as a decimal)
+# T: time the money is invested for in years
+
+# currency is in us dollars
+
+principal = 1000
+
+rate = 0.05 # 5% annual rate
+
+time = 3 # years
+
+interest = principal * rate * time
+
+print(f'The simple interest is: {interest}')
