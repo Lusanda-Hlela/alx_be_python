d5ac8dbdca8d09193c0bb9a8b044cf3082b8c12e
Lusanda <hlelalusa@gmail.com>
2024-06-23 19:54:55 +0200

    Script that calculates monthly saving and projects annual savings based on monthyl compounded interest

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,12 @@
+# Personal Finance Calculator
+
+monthlyIncome = int(input('Enter your monthly income:'))
+
+monthlyExpenses = int(input('Enter your total monthly expenses:'))
+
+monthlySavings = monthlyIncome - monthlyExpenses
+
+annualSavings = int((monthlySavings * 12) + (monthlySavings * 12 * 0.05))
+
+print(f'Your monthly savings are ${monthlySavings}.')
+print(f'Projected savings after one year, with interest, is: ${annualSavings}.')
