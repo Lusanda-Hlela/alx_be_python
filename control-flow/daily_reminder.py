3ca9e4093684b2cfe3cfe4afd9d0bcc0c8dcf671
Lusanda <hlelalusa@gmail.com>
2024-06-29 16:36:40 +0200

    Script that provides a customized reminder for a task provided by a user based on its priority and time sensitivity

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -10,14 +10,25 @@ urgency = input("Is it time-bound? (yes/no): ")
 
 
 # Provide a Customized Reminder:
+match priority:
+  case "high":
+    reminder = f"Reminder: '{task_reminder}' is a high priority task"
+    if urgency == "yes":
+      reminder += " that requires immediate attention today!"
+    else:
+      reminder += " that can wait until tomorrow!"
+  case "medium":
+    reminder = f"Reminder: '{task_reminder}' is a medium priority task"
+    if urgency == "yes":
+      reminder += " that requires some attention today."
+    else:
+      reminder += " that can wait until tomorrow!"
+  case "low":
+    reminder = f"Reminder: '{task_reminder}' is a low priority task"
+    if urgency == "yes":
+      reminder += " that must be completed today."
+    else:
+      reminder = f"Note: '{task_reminder}' is a low priority task. Consider completing it when you have free time."
 
-if urgency == "yes":
-  match priority:
-    case "high":
-      print(f"Reminder: '{task_reminder}' is a high priority task that requires immediate attention today!")
-    case "medium":
-      print(f"Reminder: '{task_reminder}' is a medium priority task that requires immediate attention today!")
-    case "low":
-      print(f"Reminder: '{task_reminder}' is a low priority task that requires immediate attention today!")
-else:
-  print(f"Note: '{task_reminder}' is a low priority task. Consider completing it when you have free time.")
+# Display the Reminder:
+print(reminder)
