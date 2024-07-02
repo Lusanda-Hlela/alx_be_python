5e05fec4cf7d01ec292f409f384fa16ef9d72ae8
Lusanda <hlelalusa@gmail.com>
2024-07-02 18:35:46 +0200

    Arithmetic Operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,10 +1,6 @@
 # Arithmetic Operations Function
 
 def perform_operation(num1, num2, operation):
-  # num1 = float(num1)
-  # num2 = float(num2)
-  # operation = str(input("Enter the operation to perform (add, subtract, multiply, divide): ")).lower()
-  
   match operation:
     case "add":
       return num1 + num2
