afae0c574ebe22eb0bc3e1aee1e5954b90de1e57
Lusanda <hlelalusa@gmail.com>
2024-07-02 22:04:28 +0200

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,12 @@
+# Shopping List Manager
+
+# Use Python lists to create a simple shopping list manager that allows users to add items, view the current list, and remove items.
+
+shopping_list = []
+
+def display_menu():
+  print("Shopping List Manager")
+  print("1. Add an item")
+  print("2. View the list")
+  print("3. Remove an item")
+  print("4. Exit")
