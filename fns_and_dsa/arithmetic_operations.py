244858986116070ab298b8637f10d2255afb70cb
Lusanda <hlelalusa@gmail.com>
2024-07-02 20:15:24 +0200

    Arithmetic Operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,5 +11,7 @@ def perform_operation(num1, num2, operation):
     case "divide":
       if num2 == 0:
         return "Cannot divide by zero."
+      elif (num1 or num2) < 0:
+        return "Only positive numbers are allowed."
       else:
         return num1 / num2
